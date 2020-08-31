39e33c15ade6300b512d5949e6c44aabdfbb1533
Daniel Brady <daniel.brady@tapjoy.com>
2020-08-31 09:35:08 -0400

    feature(table): Add `orderedPairs` sorted iteration function

--- table.lua
+++ table.lua
@@ -242,5 +242,49 @@ function module.slice(t, keys)
   return _t
 end
 
+--[[
+  Ordered table iterator, allow to iterate on the natural order of the keys of a table.
+  @see http://lua-users.org/wiki/SortedIteration
+]]
+function module.orderedPairs(t, state)
+  local function __genOrderedIndex( t )
+    local orderedIndex = {}
+    for key in next,t do
+      table.insert( orderedIndex, key )
+    end
+    table.sort( orderedIndex )
+    return orderedIndex
+  end
+
+  -- Equivalent of the next function, but returns the keys in the alphabetic
+  -- order. We use a temporary ordered key table that is stored in the
+  -- table being iterated.
+  function orderedNext(t, state)
+    local key = nil
+    if state == nil then
+      -- the first time, generate the index
+      t.__orderedIndex = __genOrderedIndex( t )
+      key = t.__orderedIndex[1]
+    else
+      -- fetch the next value
+      for i = 1,#t.__orderedIndex do
+        if t.__orderedIndex[i] == state then
+          key = t.__orderedIndex[i+1]
+        end
+      end
+    end
+
+    if key then
+      return key, t[key]
+    end
+
+    -- no more value to return, cleanup
+    t.__orderedIndex = nil
+    return
+  end
+
+  return orderedNext, t, nil
+end
+
 -----------
 return module
